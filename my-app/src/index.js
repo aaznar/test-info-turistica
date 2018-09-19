9784c880fcbac6945352a55bbdfe6cdcf4224f0a
Alejandro Aznar Alcaide <aaznar@costadelsolmalaga.org>
2018-09-19 18:17:28 +0200

    ejemplo básico

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+/* import App from './App'; */
+import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App =<Routes />
+
+ReactDOM.render(App, document.getElementById('root'));
 registerServiceWorker();
